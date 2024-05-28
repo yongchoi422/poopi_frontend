623dad84f4c2bb899c6d277f93dbca04b5635f64
yong choi <yongchoi422@gmail.com>
2024-05-28 10:49:37 +0900

    final erc-20i test connected

--- src/const/contracts.js
+++ src/const/contracts.js
@@ -1,21 +1,21 @@
 import ABIS from '@/const/abis'
 
-export const CONTRACTS = {
-  token: {
-    address: '0xAB20922092774189CAb5aB8d10D87f0Edfca0748',
-    abi: ABIS.token,
-  },
-  erc721: {
-    address: '0x638747EeFbcD6254Fc43208a5708ecbFb94F7E04',
-    abi: ABIS.erc721,
-  }
-};
+// export const CONTRACTS = {
+//   token: {
+//     address: '0xd9CA18AC7d4c392227c8C59951672bD5F584aE0f',
+//     abi: ABIS.token,
+//   },
+//   erc721: {
+//     address: '0xd9CA18AC7d4c392227c8C59951672bD5F584aE0f',
+//     abi: ABIS.erc721,
+//   }
+// };
 
 //token holder+ nft wallet TBA 주소로 my erc-20i inscription모두 조회하게 추가
 
-// export const CONTRACTS = {
-//   token: {
-//     address: '0x44Cda06D544E2E1912Bd4C82A374c3F3fF6e51CE',
-//     abi: ABIS.token,
-//   },
-// }
+export const CONTRACTS = {
+  token: {
+    address: '0xd9CA18AC7d4c392227c8C59951672bD5F584aE0f',
+    abi: ABIS.token,
+  },
+}
